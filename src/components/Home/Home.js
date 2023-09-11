10339e0a5885ed5476a83a87f3afe7c9bad18807
Aakash Acharya <mail.aakash108@gmail.com>
2023-09-12 02:09:35 +1000

    implementing redux store

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -3,13 +3,17 @@ import DefaultLayout from '../layout/DefaultLayout'
 import MovieListing from '../MovieListing/MovieListing'
 import movieApi, { fetchData } from "../../common/apis/movieApi"
 import {APIKey} from "../../common/apis/MovieApiKey"
+import { useDispatch } from 'react-redux'
+import { addMovies } from '../../features/movies/movieSlice'
 const Home = () => {
-
+  const dispatch = useDispatch()
   useEffect(()=>{
     const movieName = "Harry"
+    
     const display = async(movieName)=>{
       const response = await fetchData(movieName)
-      console.log(response)
+
+      dispatch(addMovies(response))
     }
     display(movieName)
   },[])
